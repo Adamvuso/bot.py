d060cae3937125b45b61e7f12e03ca896ec91a87
Adamvuso <67783144+Adamvuso@users.noreply.github.com>
2020-07-03 13:23:07 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,28 @@
+from aiogram import Bot, Dispatcher, executor, types
+API_TOKEN = '1324835503:AAEWZ6NY_MskhIVq4VQfnKEKwkOuO0MZT8Y'
 
+
+#Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+
+
+@dp.message_handler(commands=['start'])
+async def start(msg: types.message):
+  await msg.answer("🌬️Я ничего не умею!🌬️")
+
+
+
+@dp.message_handler(commands=['help'])
+async def echo(msg: types.message):
+  await msg.answer("Привет! Я бот Бабэтта. Пока я только умею повторять за вами, но вскоре появятся новые функции!")
+
+if __name__=='__main__':
+  executor.start_polling(dp, skip_updates=True)
+
+
+      
 
 
 
